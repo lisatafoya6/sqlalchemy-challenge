077137181f70d6ec1e8d17cee285ff01ddf14b61
lisatafoya6 <lisaa.tafoyaa@gmail.com>
2025-03-11 22:24:01 -0700

    adding forgotten app file

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,66 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """list all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    last_date = session.query(func.max(Measurement.date)).scalar()
+    one_year_ago = dt.datetime.strptime(last_date, "%Y-%m-%d") - dt.timedelta(days=365)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago).all()
+    precipitation_data = {date: prcp for date, prcp in results}
+    return jsonify(precipitation_data)
+
+@app_route("/api/v1.0/tobs<br/>")
+def tobs():
+    last_date = session.query(func.max(Measurement.date)).scalar()
+    one_year_ago = dt.datetime.strptime(last_date, "%Y-%m-%d") - dt.timedelta(days=365)
+    most_active_station = session.query(Measurement.station).group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc()).first()[0]
+    results = session.query(Measurement.date, Measurement.tobs).\
+    filter(Measurement.station == most_active_station).\
+    filter(Measurement.date >= one_year_ago).all()
+    temp_data = [{date: tobs} for date, tobs in results]
+    return jsonify(temp_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
